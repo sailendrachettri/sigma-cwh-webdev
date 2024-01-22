1810822da73faf519ceb4f882784181bfd9b13ac
sailendrachettri <sailendra9083@gmail.com>
2024-01-22 17:20:34 +0530

    navbar 7 complected - 15 minutes

---
+++ navbar7/script.js
@@ -0,0 +1,8 @@
+let hamburger, navbar;
+
+hamburger = document.querySelector('.hamburger');
+navbar = document.querySelector('.navbar');
+
+hamburger.onclick = function(){
+    navbar.classList.toggle('active');
+}
