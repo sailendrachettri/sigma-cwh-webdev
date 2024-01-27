d6113301d2a57ff9522fd55e122446236e302b61
sailendrachettri <sailendra9083@gmail.com>
2024-01-27 20:12:23 +0530

    simple registration form

--- navbar14/script.js
+++ navbar14/script.js
@@ -11,3 +11,26 @@ document.querySelectorAll('.nav-item').forEach(n => n.addEventListener("click", 
     hamburger.classList.remove("active");
 }))
 
+// -------------------------get form data-------------------------
+let firstName, lastName, email, password, confirmPassword, country;
+
+firstName = document.getElementById('first-name');
+lastName = document.getElementById('last-name');
+email = document.getElementById('email');
+password = document.getElementById('password');
+confirmPassword = document.getElementById('confirm-password');
+country = document.getElementById('country');
+
+function register(){
+        if(firstName.value.length < 3)
+            alert("First name length should be greater than 3.");
+        else if(password.value.length < 6)
+            alert("Password length should greater than 6.")
+        else if((password.value) !== (confirmPassword.value))
+            alert("Passowrd doesn't match");
+        
+        else{
+            let main = document.getElementById('main');
+            main.innerHTML = "Thanks for registration.";
+        } 
+}
